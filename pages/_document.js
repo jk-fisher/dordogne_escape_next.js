78f79a9dd98ae60a9e55a4a0bf303b603d6cce7f
Joanna Fisher <fisherjoanna@sky.com>
2022-08-16 19:02:22 +0200

    Fix crossOrigin bug

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MainDocument extends Document {
-  // static async getInitialProps(ctx) {
-  //   const initialProps = await Document.getInitialProps(ctx);
-  //   return { ...initialProps };
-  // }
 
   render() {
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
           <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"/>
         </Head>
         <body>
