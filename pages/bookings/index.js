44ba9ca3c2be2294219bef7137ff9684b7e3940a
jk-fisher <fisherjoanna@sky.com>
2023-02-25 18:42:57 +0100

    Add capital letter to Bookings page comp

--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -5,28 +5,31 @@ import Bookings from "../../components/Bookings/Bookings";
 import ModalContext from "../../store/modal-context";
 import useModal from "../../hooks/useModal";
 
-const bookings = () => {
-    const { showModal, openModalHandler, closeModalHandler, modalContent } = useModal();
-    return ( 
-        <Fragment>
-            <Head>
-                <title>Le Petit Cottage</title>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta
-                    name="description"
-                    content="Le Petit Cottage, self-catered accommodation in France, available to book for holidays to the Dordogne region."
-                />
-                <meta
-                    name="keywords" 
-                    content="Le Petit Cottage, Dordogne, France, holiday, rental, self-catered, accomodation"
-                />
-            </Head>
-            <ModalContext.Provider value={{ showModal, openModalHandler, closeModalHandler, modalContent }}>
-                <Bookings />
-            </ModalContext.Provider>
-        </Fragment>
-     );
-}
- 
-export default bookings;
+const Bookings = () => {
+  const { showModal, openModalHandler, closeModalHandler, modalContent } =
+    useModal();
+  return (
+    <Fragment>
+      <Head>
+        <title>Le Petit Cottage</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Le Petit Cottage, self-catered accommodation in France, available to book for holidays to the Dordogne region."
+        />
+        <meta
+          name="keywords"
+          content="Le Petit Cottage, Dordogne, France, holiday, rental, self-catered, accomodation"
+        />
+      </Head>
+      <ModalContext.Provider
+        value={{ showModal, openModalHandler, closeModalHandler, modalContent }}
+      >
+        <Bookings />
+      </ModalContext.Provider>
+    </Fragment>
+  );
+};
+
+export default Bookings;
